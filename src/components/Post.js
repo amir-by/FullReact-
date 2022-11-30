b128381d51bf5b97dce6ce14c8554c98b8131fee
amir-by <amirmeshket@gmail.com>
2022-11-30 15:38:28 +0100

    fake API

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,20 +1,20 @@
 import React from 'react'
  
-const Post = ({item,handleDelete}) => {
+const Post = ({post,handleDelete}) => {
 
 
    return (
     <div className='my-6 card p-4 '>
-      <img src={item.img} alt={item.id}
+      <img src={post.img} alt={post.id}
       className='image' />
 
       <div className='iflex mt-2'>
-        <h1 className='text-lg font-semibold uppercase'>{item.title}</h1>
+        <h1 className='text-lg font-semibold uppercase'>{post.title}</h1>
         {/* Delete btn */}
-        <button onClick={()=>handleDelete(item.id)} className='btn'>Delete</button>
+        <button onClick={()=>handleDelete(post.id)} className='btn'>Delete</button>
       </div>
 
-      <h2>By: <span className='text-red-800 font-bold'> {item.author? item.author :'Amir ♥'}</span> </h2>
+      <h2>By: <span className='text-red-800 font-bold'> {post.author? post.author :'Amir ♥'}</span> </h2>
       <p className='text-sm text-gray-500'>Lorem ipsum dolor sit amet consectetur.</p>
     </div>
   )
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Post from './Post'
 import src1 from "../assets/borabora.jpg";
 import src2 from "../assets/maldives.jpg";
@@ -6,25 +6,27 @@ import src3 from "../assets/keywest.jpg";
 
 const Posts = () => {
 
-  const myData = [
-    {id:1 , title:"title 1" , img:src1 , author:"Admin" },
-    {id:2 , title:"title 2" , img:src2 , author:"User" },
-    {id:3 , title:"title 3" , img:src3 ,  },
-  ]
-
-  const [data, setData] = useState(myData);
+  //stock data
+  const [posts, setPosts] = useState(null);
+  //fetch Data from fake API
+  useEffect(()=>{
+    fetch("http://localhost:4000/posts")
+    .then((res)=>{return res.json()})
+    .then((data)=>{return console.log(data); })
+   }, [])
   
+
+  //delete cardItem
   const handleDelete = (id) => {
-   
-    const copyData = [...data]
-    setData(copyData.filter(post=>(post.id !== id)))
+    const copyData = [...posts]
+    setPosts(copyData.filter(post=>(post.id !== id)))
   }
 
   return (
     <section className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8  justify-between max-w-[1240px] m-auto py-8 px-4 '>
-        {data.map((item,i)=>(
-          <Post key={i} item={item} handleDelete={handleDelete} />
-        ))}
+        {posts ? posts.map((post,i)=>(
+          <Post key={i} post={post} handleDelete={handleDelete} />
+        )): 'no posts'}
     
     </section>
   )
